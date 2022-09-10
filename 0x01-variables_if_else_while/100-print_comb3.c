96e34b6719fc37bb3460d863843665c87f563a2c
bbiee <debbieoparaji@gmail.com>
2022-09-10 03:00:14 -0700

    100print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,53 +4,35 @@
  *  main - entry point
  *
  *  Return: alway returns 0
+ *
  */
 
 int main(void)
 {
+	int i;
+	int j;
+	int k;
 
-		int i;
-
-		int j;
-
-
-
-		for (i = 10; i <= 19; i++)
-
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
 		{
-
-			for (j = 10; j <= 19; j++)
-
+			for (k = 0; k <= 9; k++)
 			{
-
-				if ((j % 10) > (i % 10))
-
+				if (k > j && j > i)
 				{
-
-																						putchar((i % 10) + '0');
-
-																						putchar((j % 10) + '0');
-
-																						if (i != 18 || j != 19)
-
-																						{
-
-																																								putchar(',');
-
-																																								putchar(' ');
-
-																																							}
-
-																					}
-
-
-
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i != 7 || j != 8 || k != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
-
 		}
-
-		putchar('\n');
-
-		return (0);
-
+	}
+	putchar('/n');
+	return (0);
 }
